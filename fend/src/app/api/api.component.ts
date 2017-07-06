4c3354e7057ef1b01d4eed98472b966ea0e97dc2
Victor Pino <victopin0@gmail.com>
2017-07-06 15:02:26 -0400

    feat(fendun/api): Add User model and validation to formUser

--- fend/src/app/api/api.component.ts
+++ fend/src/app/api/api.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
     selector: 'api',
@@ -7,12 +8,19 @@ import { Component } from '@angular/core';
 export class ApiComponent{
 
 	public title:string;
+	public user: User;
 
 	constructor(){
 		this.title = 'API';
+		this.user = new User("", "");
 	}
 
 	ngOnInit(){
 		console.log("Cargado el componente");
+	}
+
+	checkUser(){
+		console.log(this.user);
+		this.user = new User("", "");
 	}
 }
