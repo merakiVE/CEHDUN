47683f59d1ac3a1a62c2e9fea1f4a116d5017517
Israel Lugo <hostelixisrael@gmail.com>
2017-09-07 22:48:08 -0400

    update(cehdun): add comments to code

--- core/parser/parser.go
+++ core/parser/parser.go
@@ -1,11 +1,17 @@
 package parser
 
+/* Constantes en donde se definen los proveedores de parseo */
 const (
 	PROVIDER_BPMNIO_DIAGRAM = "bpmn.io"
 	PROVIDER_GOJS_DIAGRAM   = "gojs"
 	PROVIDER_DRAWIO_DIAGRAM = "draw.io"
 )
 
+/*
+	Funcion que sirve para instanciar el nuevo parseador dependiendo del proveedor que se especifique
+
+	Retorna una interface{}, los metodos de esta deberian coincidir con la interface Diagram
+ */
 func NewProviderParser(provider string) (interface{}) {
 
 	switch provider {
