f07759f3ce649a837850f92903f564cc43f0b94f
Israel Lugo <hostelixisrael@gmail.com>
2017-09-07 23:41:40 -0400

    update(cehdun): testing new methods interface Diagram in process save

--- core/processor/diagram.go
+++ core/processor/diagram.go
@@ -1,61 +1,39 @@
 package processor
 
-import "github.com/merakiVE/CVDI/src/models"
 import (
+	"github.com/merakiVE/CVDI/src/models"
 	"github.com/merakiVE/CEHDUN/core/parser"
 	"github.com/merakiVE/CVDI/core/db"
 )
 
-type Gateway struct {
-	Type string
-}
-
-type Event struct {
-	Type string
-}
-
-type Activity struct {
-	Name string
-}
-
-type Diagram interface {
-	GetGateways() []Gateway
-	GetEvents() []Event
-	GetActivities() []Activity
-	//GetLanes()
-	//GetPools()
-}
-
-func DiagramProcessor() {
-
-}
+func DiagramProcessor() {}
 
 func VerifyDiagram() {}
 
 func SaveDiagram() ([]map[string]string) {
-	diagram := parser.NewDiagram()
-	diagram.ReadFromFile("/home/hostelix/pruebas_go/diagram.bpmn")
+
+	diagram := parser.NewProviderParser(parser.PROVIDER_BPMNIO_DIAGRAM)
+	diagram.LoadDiagramByPath("diagram.bpmn")
 
 	m := models.ProcedureModel{}
 
-	for index, activity := range diagram.GetSuccessionProcess() {
+	for index, activity := range diagram.GetActivities() {
 
 		m.Activities = append(m.Activities, models.Activity{
-			Name:     diagram.GetAttributeElement(activity, parser.ATTR_NAME),
+			Name:     activity.Name,
 			Sequence: index + 1,
 		})
 	}
 
 	for _, lane := range diagram.GetLanes() {
 		m.Lanes = append(m.Lanes, models.Lane{
-			Name: diagram.GetAttributeElement(lane, parser.ATTR_NAME),
+			Name: lane.Name,
 		})
 	}
 
 	m.Owner = "owner_test"
 
 	db.SaveModel(db.GetCurrentDatabase(), &m)
-
 
 	return m.GetValidationErrors()
 
