e24025745deef63f8f23357893d94ded2974563a
Israel Lugo <hostelixisrael@gmail.com>
2017-09-07 20:42:04 -0400

    update(parser/types):add types to bpmnio diagram

--- core/parser/types.go
+++ core/parser/types.go
@@ -1,1 +1,21 @@
 package parser
+
+type Gateway struct {
+	Type string
+}
+
+type Event struct {
+	Type string
+}
+
+type Activity struct {
+	Name string
+}
+
+type Diagram interface {
+	GetGateways() []Gateway
+	GetEvents() []Event
+	GetActivities() []Activity
+	//GetLanes()
+	//GetPools()
+}
