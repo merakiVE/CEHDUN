5b169daeed309e629fa060bc8d6e7b7f911a8ad2
Israel Lugo <hostelixisrael@gmail.com>
2017-09-07 22:30:26 -0400

    update(cehdun/parser): add new function NewProviderParser

--- core/parser/parser.go
+++ core/parser/parser.go
@@ -1,1 +1,26 @@
 package parser
+
+const (
+	PROVIDER_BPMNIO_DIAGRAM = "bpmn.io"
+	PROVIDER_GOJS_DIAGRAM   = "gojs"
+	PROVIDER_DRAWIO_DIAGRAM = "draw.io"
+)
+
+func NewProviderParser(provider string) (interface{}) {
+
+	switch provider {
+
+	case PROVIDER_BPMNIO_DIAGRAM:
+		return NewParserBPMNIO()
+
+	case PROVIDER_GOJS_DIAGRAM:
+		return nil
+
+	case PROVIDER_DRAWIO_DIAGRAM:
+		return nil
+
+	default:
+		return NewParserBPMNIO()
+	}
+	return nil
+}
