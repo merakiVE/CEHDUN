d8116f3f8babb758a0ab04a8c9f8d652d4f8ad76
Israel Lugo <hostelixisrael@gmail.com>
2017-09-08 12:21:52 -0400

    update(cehdun/utils): add validation key is empty

--- core/utils/utils.go
+++ core/utils/utils.go
@@ -1,6 +1,12 @@
 package utils
 
+import "strings"
+
 func InSlice(key string, slice_str []string) (bool) {
+	if len(strings.TrimSpace(key)) == 0 {
+		return false
+	}
+
 	for _, value := range slice_str {
 		if value == key {
 			return true
