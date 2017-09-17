b2dd6293575b9c4a91658ab703abb08856561b6d
Israel Lugo <hostelixisrael@gmail.com>
2017-09-17 01:20:32 -0400

    update(src/controllers): new modification for action ConnectDb

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -2,13 +2,21 @@ package controllers
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+
+	"github.com/merakiVE/CEHDUN/common"
+	"github.com/merakiVE/CEHDUN/core/types"
+
+	"fmt"
 )
 
-func ConnectDB(ctx iris.Context) {
+func ConnectDB(ctx context.Context) {
 
 	var dataDB types.DataBase
 
-	err = _context.ReadJSON(&dataDB)
+	err := ctx.ReadJSON(&dataDB)
+
+	fmt.Println(dataDB)
 
 	if err != nil {
 		ctx.StatusCode(iris.StatusInternalServerError)
@@ -22,7 +30,19 @@ func ConnectDB(ctx iris.Context) {
 	
 	response := common.GetData(dataDB)
 
-	result, _ := response.(types.BaseData)
+	result, errr := response.(types.BaseData)
+
+	fmt.Println(response)
+
+	if !errr {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.JSON(types.ResponseAPI{
+			Message: "Invalid data",
+			Data:    nil,
+			Errors:  nil,
+		})
+		return
+	} 
 
 	ctx.StatusCode(iris.StatusOK)
 	ctx.JSON(types.ResponseAPI{
