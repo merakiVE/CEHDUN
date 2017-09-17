7a8270abef9750eafbd30eab75801c94041a2935
Israel Lugo <hostelixisrael@gmail.com>
2017-09-16 23:52:40 -0400

    update(src/controllers): add new action for connect db

--- src/controllers/controllers.go
+++ src/controllers/controllers.go
@@ -1,1 +1,33 @@
 package controllers
+
+import (
+	"github.com/kataras/iris"
+)
+
+func ConnectDB(ctx iris.Context) {
+
+	var dataDB types.DataBase
+
+	err = _context.ReadJSON(&dataDB)
+
+	if err != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		ctx.JSON(types.ResponseAPI{
+			Message: "Invalid data",
+			Data:    nil,
+			Errors:  nil,
+		})
+		return
+	}
+	
+	response := common.GetData(dataDB)
+
+	result, _ := response.(types.BaseData)
+
+	ctx.StatusCode(iris.StatusOK)
+	ctx.JSON(types.ResponseAPI{
+		Message: "Connection success",
+		Data:    result,
+		Errors:  nil,
+	})
+}
